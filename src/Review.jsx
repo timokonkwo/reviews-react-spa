d85f540a3c366d20145eebe0e3a7082fa58e50b3
Tim <chibuiketimokonkwo@gmail.com>
2023-01-31 23:41:23 +0100

    setups

---
+++ src/Review.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+
+const Review = () => {
+	return (
+    <h2>My component</h2>
+  )
+};
+
+export default Review;
